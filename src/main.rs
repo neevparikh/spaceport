83403bff73cbc083de8881cd88a4a530895b98cd
Neev Parikh <>
2024-07-19 19:35:48 -0700

    feat: non-daemon version working

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,10 @@
 use anyhow::{anyhow, Result};
-use objc2::rc::{Allocated, Retained};
-use objc2::runtime::ProtocolObject;
-use objc2::{declare_class, msg_send_id, mutability, ClassType, DeclaredClass};
-use objc2_core_location::{CLLocationManager, CLLocationManagerDelegate};
+use objc2::rc::Retained;
+use objc2_core_location::{CLAuthorizationStatus, CLLocationManager};
 use objc2_core_wlan::{CWInterface, CWWiFiClient};
-use objc2_foundation::{NSObject, NSObjectProtocol};
 use serde::Serialize;
-
-declare_class!(
-    struct LocationManagerDelegate;
-
-    unsafe impl ClassType for LocationManagerDelegate {
-        type Super = NSObject;
-        type Mutability = mutability::InteriorMutable;
-        const NAME: &'static str = "LocationManagerDelegate";
-    }
-
-    impl DeclaredClass for LocationManagerDelegate {}
-
-    unsafe impl LocationManagerDelegate {
-        #[method_id(init)]
-        fn init(this: Allocated<Self>) -> Option<Retained<Self>> {
-            let this = this.set_ivars(());
-            unsafe { msg_send_id![super(this), init] }
-        }
-    }
-
-    unsafe impl NSObjectProtocol for LocationManagerDelegate {}
-
-    unsafe impl CLLocationManagerDelegate for LocationManagerDelegate {}
-);
-
-impl LocationManagerDelegate {
-    pub fn new() -> Retained<Self> {
-        unsafe { msg_send_id![Self::alloc(), init] }
-    }
-}
+use std::thread::sleep;
+use std::time::Duration;
 
 #[derive(Debug, Default, Serialize)]
 struct Wifi {
@@ -48,7 +17,7 @@ struct Wifi {
 }
 
 impl Wifi {
-    unsafe fn from_interface(interface: Retained<CWInterface>) -> Self {
+    unsafe fn from_interface(interface: &Retained<CWInterface>) -> Self {
         Wifi {
             ssid: interface.ssid().map(|s| s.to_string()),
             power: interface.powerOn(),
@@ -58,24 +27,29 @@ impl Wifi {
             tx_power: interface.transmitPower(),
         }
     }
+
+    fn to_json(&self) -> Result<String> {
+        let json = serde_json::to_string_pretty(&self)?;
+        Ok(json)
+    }
 }
 
 fn main() -> Result<()> {
-    let wifi = unsafe {
+    let interface = unsafe {
         let manager = CLLocationManager::new();
-        let delegate = LocationManagerDelegate::new();
-        let protocol = ProtocolObject::from_ref(delegate.as_ref());
-
-        manager.setDelegate(Some(protocol));
+        manager.requestAlwaysAuthorization();
         manager.startUpdatingLocation();
 
-        dbg!(manager.delegate());
+        while manager.authorizationStatus() != CLAuthorizationStatus(3) {
+            sleep(Duration::from_millis(10))
+        }
 
-        let interface = CWWiFiClient::sharedWiFiClient().interface();
-        interface.map(|interface| Wifi::from_interface(interface))
+        CWWiFiClient::sharedWiFiClient().interface()
     }
     .ok_or(anyhow!("Unable to get wifi interface"))?;
-    let json = serde_json::to_string_pretty(&wifi)?;
-    println!("{}", json);
+
+    let wifi = unsafe { Wifi::from_interface(&interface) };
+
+    println!("{}", wifi.to_json()?);
     Ok(())
 }
